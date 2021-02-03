f9da5ef206f3707d36a4a4bd7934c379eeff1566
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-02-03 10:36:49 -0300

    refatoring file

--- Modules/Modulo_01/Javascript Testing/Aula_01/mocks-01/src/file.js
+++ Modules/Modulo_01/Javascript Testing/Aula_01/mocks-01/src/file.js
@@ -1,26 +1,59 @@
-
-// ler arquivos
 const { readFile } = require('fs/promises');
-const { join } = require('path')
+const { error } = require('./constants')
+
+const defaultOption = {
+  maxLines: 3,
+  fields: ['id','name','profession', "age"]
+}
 
 class File {
 
   static async csvToJson(filePath) {
       const content = await File.getFileContent(filePath)
-    return content
+      const validation = File.isValid(content)
+      if (!validation.valid) throw new Error(validation.error)
+      return content
   }
 
   static async getFileContent(filePath) {
-      //normaliza a porta
-      const fileName = join(__dirname, filePath)
-
-      return (await readFile(fileName)).toString('utf-8')
+      return (await readFile(filePath)).toString('utf-8')
   }
+
+  static isValid(csvString, options = defaultOption) {
+    const [header, ...fileWithOutHeader] = csvString.split('\n')
+
+    // console.log('header', header, 'file', fileWithOutHeader);
+    const isHeaderValid = header === options.fields.join(',')
+        if (!isHeaderValid) {
+         return {
+              error: error.FILE_FIELDS_ERROR_MESSAGE,
+              valid: false,
+      }
+    }
+    const isContentLengthAccept = (
+      fileWithOutHeader.length > 0 &&
+      fileWithOutHeader.length <= options.maxLines
+    )
+
+    if (!isContentLengthAccept) {
+      return {
+        error: error.FILE_LENGTH_ERROR_MESSAGE,
+        valid: false
+      }
+      
+    }
+
+    return { valid: true }
+  }
+
 }
 
-// funcao main que inicia o codigo
+// (async () => {
+//   // const result = await File.csvToJson('./../mocks/threeeItens-valid.csv');
+//   // const result = await File.csvToJson('./../mocks/fuorItens-invalid.csv');
+//   const result = await File.csvToJson('./../mocks/invalid-header.csv');
+//   console.log('result', result);
+// })();
 
-(async () => {
-  const result = File.csvToJson('../../mocks/emptyFile-invalid.csv');
-  console.log('result', result);
-})();
+
+module.exports = File;
