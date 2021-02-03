2b989d003b92d742a607c9ccd88898290f552206
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-02-03 11:06:25 -0300

    add paths

---
+++ Modules/Modulo_01/Javascript Testing/Aula_01/mocks-01/src/utils/paths.js
@@ -0,0 +1,9 @@
+const path = {
+    invalid: './mocks/emptyFile-invalid.csv',
+    fuorInvalid: './mocks/fuorItens-invalid.csv',
+    invalidHeader: './mocks/invalid-header.csv',
+    threeItensValid: './mocks/threeeItens-valid.csv'
+
+}
+
+module.exports = path
