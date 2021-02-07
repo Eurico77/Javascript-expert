072c4086b40e8eb545b115f21dc0bfdb5ebef8df
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-02-07 00:19:05 -0300

    add validacao dos dados e metodos para filtrar dados

--- Modules/Modulo_01/Javascript Testing/Aula_01/mocks-01/index.test.js
+++ Modules/Modulo_01/Javascript Testing/Aula_01/mocks-01/index.test.js
@@ -1,52 +1,48 @@
 const File = require('./src/file')
 const { error } = require('./src/constants')
-const { rejects, deepStrictEqual } = require('assert')
+const assert = require('assert')
 const path = require('./src/utils/paths')
 
 
 ;
 (async () => {
-{   
-    const filePath = path
-    const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
-    const result = File.csvToJson(filePath.invalid)
-    await rejects(result, rejection)
-}
-{
-  const filePath = path
-  const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
-  const result = File.csvToJson(filePath.fuorInvalid)
-  await rejects(result, rejection)
-}
+// {   
+//     const filePath = path
+//     const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
+//     const result = File.csvToJson(filePath.invalid)
+//     await rejects(result, rejection)
+// }
+// {
+//   const filePath = path
+//   const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
+//   const result = File.csvToJson(filePath.fuorInvalid)
+//   await rejects(result, rejection)
+// }
 {
   const filePath = './mocks/threeItems-valid.csv'
   const result = await File.csvToJson(filePath)
   const expected = [
-    
-      {
-        "id": 123,
-        "name": "Erick Wendel",
-        "profession": "Javascript Instructor",
-        "age": 25
-      },
-      {
-        "id": 321,
-        "name": "Xuxa da Silva",
-        "profession": "Javascript Specialist",
-        "age": 80
-      },
-      {
-        "id": 231,
-        "name": "Joaozinho",
-        "profession": "Java Developer",
-        "age": 30
-      }
+    {
+      "name": "Erick Wendel",
+      "id": 123,
+      "profession": "Javascript Instructor",
+      "birthDay": 1995
+  },
+  {
+      "name": "Xuxa da Silva",
+      "id": 321,
+      "profession": "Javascript Specialist",
+      "birthDay": 1940
+  },
+  {
+      "name": "Joaozinho",
+      "id": 231,
+      "profession": "Java Developer",
+      "birthDay": 1990
+  }
     
   ]
-  
-  deepStrictEqual(result, expected)
-
+  assert.deepStrictEqual(JSON.stringify(result), JSON.stringify(expected))
 }
-
   })();
   
