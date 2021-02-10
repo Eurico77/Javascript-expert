25e3cf453cfcdc51818dedba8d90ad2d8d71783b
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-02-09 22:10:04 -0300

    refatoring

--- Modules/Modulo_01/Javascript Testing/Aula_01/stubs/src/index.test.js
+++ Modules/Modulo_01/Javascript Testing/Aula_01/stubs/src/index.test.js
@@ -1,51 +1,42 @@
-const Service = require('./service');
-const sinon = require('sinon')
-const { deepStrictEqual } = require('assert')
-const BASE_URL_1 = 'https://swapi.dev/api/planets/1/'
-const BASE_URL_2 = 'https://swapi.dev/api/planets/3/'
+const Service = require("./service");
+const sinon = require("sinon");
+const { deepStrictEqual } = require("assert");
+const BASE_URL_1 = "https://swapi.dev/api/planets/1/";
+const BASE_URL_2 = "https://swapi.dev/api/planets/3/";
 const mocks = {
-    tatooine: require('./mocks/totooine.json'),
-    yavinIV: require('./mocks/yavinIV.json')
-}
-;
+  tatooine: require("./mocks/totooine.json"),
+  yavinIV: require("./mocks/yavinIV.json"),
+};
 (async () => {
+//   {
+ 
+//         const service = new Service()
+//         const withhoutStub = await service.makeRequest(BASE_URL_2)
+//         console.log(JSON.stringify(withhoutStub));
+//   }
 
-    {
-        // usando a internet
-    //     const service = new Service()
-    //     const withhoutStub = await service.makeRequest(BASE_URL_2)
-    //     console.log(JSON.stringify(withhoutStub));
-    }
+  const service = new Service();
+  const stub = sinon.stub(service, service.makeRequest.name);
 
+  stub.withArgs(BASE_URL_1).resolves(mocks.tatooine);
+  stub.withArgs(BASE_URL_2).resolves(mocks.yavinIV);
 
-    const service = new Service()
-    const stub = sinon.stub(service, service.makeRequest.name)
-
-        stub
-            .withArgs(BASE_URL_1)
-            .resolves(mocks.tatooine)
-        stub
-            .withArgs(BASE_URL_2)
-            .resolves(mocks.yavinIV)
-
-            {
-                const expected ={
-                    "name": "Tatooine",
-                    "surface_water": "1",
-                    aparedIn: 5
-                }
-                const res = await service.getPlanets(BASE_URL_1)
-                deepStrictEqual(res, expected)
-                
-            }
-            {
-                const expected ={
-                    "name": "Yavin IV",
-                    "surface_water": "8",
-                    aparedIn: 1
-                }
-                const res = await service.getPlanets(BASE_URL_2)
-                deepStrictEqual(res, expected)
-            } 
-            
-})()
+  {
+    const expected = {
+      name: "Tatooine",
+      surface_water: "1",
+      aparedIn: 5,
+    };
+    const res = await service.getPlanets(BASE_URL_1);
+    deepStrictEqual(res, expected);
+  }
+  {
+    const expected = {
+      name: "Yavin IV",
+      surface_water: "8",
+      aparedIn: 1,
+    };
+    const res = await service.getPlanets(BASE_URL_2);
+    deepStrictEqual(res, expected);
+  }
+})();
--- Modules/Modulo_01/Javascript Testing/Aula_01/stubs/src/service.js
+++ Modules/Modulo_01/Javascript Testing/Aula_01/stubs/src/service.js
@@ -1,25 +1,24 @@
-const https = require('https');
+const https = require("https");
 
 class Service {
-    async makeRequest(url) {
-        return new Promise((resolve, reject) => {
-            https.get(url, response => {
-                response.on("data", data => resolve(JSON.parse(data)))
-                response.on("error", reject)
-            })
-        })
-    }
-    async getPlanets(url) {
-        const result = await this.makeRequest(url)
+  async makeRequest(url) {
+    return new Promise((resolve, reject) => {
+      https.get(url, (response) => {
+        response.on("data", (data) => resolve(JSON.parse(data)));
+        response.on("error", reject);
+      });
+    });
+  }
+  
+  async getPlanets(url) {
+    const { name, surface_water, films } = await this.makeRequest(url);
 
-        return {
-            name: result.name,
-            surface_water: result.surface_water,
-            aparedIn: result.films.length,
-        }
-    }
+    return {
+      name,
+      surface_water,
+      aparedIn: films.length,
+    };
+  }
 }
 
-
-
-module.exports = Service
+module.exports = Service;
