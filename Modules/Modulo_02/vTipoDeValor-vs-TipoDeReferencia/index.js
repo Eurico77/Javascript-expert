a0080ca0a12163ee616c3763550d3b619a69a4a4
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-02-28 22:04:13 -0300

    fazendo acercoes sobre referencia de tipos e tipos de valores

--- Modules/Modulo_02/vTipoDeValor-vs-TipoDeReferencia/index.js
+++ Modules/Modulo_02/vTipoDeValor-vs-TipoDeReferencia/index.js
@@ -1,4 +1,6 @@
+const { deepStrictEqual } = require("assert");
 /*
+
 Primeiro exemplo
 copia o endereço mas não altera o valor da primeira variavel instanciada
 pois os tipos primitivos são armazenados de uma forma diferente das demais variavéis
@@ -16,4 +18,14 @@ copia e altera pois a referencia é a mesma
 const item = { counter: 0 };
 const item2 = item;
 
+// o tipo primitivo gera uma copia em memoria
+deepStrictEqual(counter, 0);
+deepStrictEqual(counter2, 1);
+
+// tipo de referência copia o endereço de memoria
+// e aponta pro mesmo lugar
+
 item2.counter++;
+deepStrictEqual(item, { counter: 1 });
+
+deepStrictEqual(item, { counter: 2 })
