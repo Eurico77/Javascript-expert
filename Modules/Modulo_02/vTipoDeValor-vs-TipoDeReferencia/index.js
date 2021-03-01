963b9cc20b8a5c35ae776046a9a0f56429df3a6d
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-02-28 21:57:54 -0300

    comecando modulo 2 do bootcamp

--- Modules/Modulo_02/vTipoDeValor-vs-TipoDeReferencia/index.js
+++ Modules/Modulo_02/vTipoDeValor-vs-TipoDeReferencia/index.js
@@ -1,24 +1,19 @@
 /*
 Primeiro exemplo
-
-
 copia o endereço mas não altera o valor da primeira variavel instanciada
 pois os tipos primitivos são armazenados de uma forma diferente das demais variavéis
 */
-let counter = 0
-let counter2 = counter
+let counter = 0;
+let counter2 = counter;
 
-counter2++
-
+counter2++;
 
 /*
 Segundo exemplo
-
-
-copia o endereço 
+copia e altera pois a referencia é a mesma 
 */
 
-const item = {counter: 0}
-const item2 = item
+const item = { counter: 0 };
+const item2 = item;
 
-item2.counter ++
+item2.counter++;
