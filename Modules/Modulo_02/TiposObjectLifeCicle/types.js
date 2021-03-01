f3782b68cdb2e37033a7539e4a2dde0cf618c7c7
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-02-28 22:55:10 -0300

    add tipos e comparacoes

--- Modules/Modulo_02/TiposObjectLifeCicle/types.js
+++ Modules/Modulo_02/TiposObjectLifeCicle/types.js
@@ -1,6 +1,16 @@
-'1' == 1 //true
-'1' === 1 // false
+"1" == 1; //true
+"1" === 1; // false
 
+console.assert(String(123) === "123", "convsersão explicita para string");
+console.assert(123 + "" === "123", "convsersão implicita para string");
 
-console.assert(String(123) === '123', "convsersão explicita para string");
-console.assert(123 + '' === '123', "convsersão implicita para string");
+console.assert(
+  ("hello" || 123) === "hello",
+  "|| retorna o primeiro elemento se os dois forem true"
+);
+
+console.assert(
+  ("hello" && 123) === "hello",
+  "|| retorna o ultimo elemento se os dois forem true"
+);
+
