1744e10bd0408eabd55338456497a6e72c3b48ed
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-03-01 23:58:31 -0300

    avancando nos tipos

--- Modules/Modulo_02/TiposObjectLifeCicle/Objects.js
+++ Modules/Modulo_02/TiposObjectLifeCicle/Objects.js
@@ -4,15 +4,29 @@ const person = {
 
   //STRING: 1 se não for primitivo, chama o valueOf()
     toString() {
+        // console.log('opa, passei aqui');
         return `Name: ${this.name}, Age: ${this.age}`
     },
     //NUMBER: 1 se não for primitivo, chama o toString()
     valueOf() {
-        return 224
+        // return 224
+        return { hey: 'Dude' }
+    },
+    
+    //ele manda em tudo!!!
+    [Symbol.toPrimitive](coercionType) {
+        console.log('trying to convert to', coercionType);
+
+        const typpes = {
+
+        }
     }
 };
 
 // console.log(''.concat(person));
 
-console.log('toString:',String(person));
-console.log('valueOf',Number(person));
+// console.log('toString:',String(person));
+// console.log('valueOf', Number(person));
+
+// depois de add o primitive
+console.log('String');
